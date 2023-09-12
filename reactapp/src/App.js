d1dba3af6d26a0ca23e71961f9f2d4c9087db22c
Iamneo <techadmin@iamneo.ai>
2023-09-12 13:03:32 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+// App.js
+import React, { useState } from 'react';
 import './App.css';
+import ProductList from './components/ProductList';
+import ProductDetail from './components/ProductDetail';
+import Cart from './components/Cart';
+import Filter from './components/Filter';
+import productsData from './data/products';
 
 function App() {
+  const [selectedProduct, setSelectedProduct] = useState(null);
+  const [cartItems, setCartItems] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState(productsData);
+
+  const handleProductSelect = (product) => {
+    setSelectedProduct(product);
+  };
+
+  const handleAddToCart = (product) => {
+    const updatedCart = [...cartItems, product];
+    setCartItems(updatedCart);
+  };
+
+  const handleRemoveFromCart = (product) => {
+    const updatedCart = cartItems.filter((item) => item.id !== product.id);
+    setCartItems(updatedCart);
+  };
+
+  const handleFilter = (priceRange) => {
+    // Filter products based on price range
+    const filtered = productsData.filter((product) => {
+      const price = parseFloat(product.price);
+      const min = parseFloat(priceRange.min);
+      const max = parseFloat(priceRange.max);
+      return (!min || price >= min) && (!max || price <= max);
+    });
+
+    setFilteredProducts(filtered);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>E-Commerce Store</h1>
+      <div className="container">
+        <div className="left-panel" data-testid="product-details" >
+          <Filter onFilter={handleFilter} />
+          <ProductList products={filteredProducts} onProductSelect={handleProductSelect} />
+        </div>
+        <div className="right-panel" data-testid="cart-details" >
+          <ProductDetail product={selectedProduct} />
+          <Cart cartItems={cartItems} onRemoveItem={handleRemoveFromCart} />
+        </div>
+      </div>
     </div>
   );
 }
